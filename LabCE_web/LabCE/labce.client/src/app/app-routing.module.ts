e52cca29707cdd79afc944a0ac4b74f3299b036c
Albert Vega camacho <85046754+AlbertVega@users.noreply.github.com>
2024-04-29 17:34:32 -0600

    operator view with side-navbar
    Addition of the operator view with a responsive and animated side-navbar

--- LabCE_web/LabCE/labce.client/src/app/app-routing.module.ts
+++ LabCE_web/LabCE/labce.client/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OperatorLoginComponent } from './operator-login/operator-login.component';
+import { OperatorLoginComponent } from './components/operator-login/operator-login.component';
+import { OperatorViewComponent } from './components/operator-view/operator-view.component';
+import { PrestamoActivoComponent } from './components/operator-view/prestamo-activo/prestamo-activo.component';
+import { RegDevolucionComponent } from './components/operator-view/reg-devolucion/reg-devolucion.component';
+import { ReservaLabComponent } from './components/operator-view/reserva-lab/reserva-lab.component';
+import { ReportesComponent } from './components/operator-view/reportes/reportes.component';
 
 const routes: Routes = [
   
   {path: 'op-login', component: OperatorLoginComponent},
+  {
+    path: 'op-view', 
+    component: OperatorViewComponent, 
+    children: [
+      {path: 'prestamo-activo', component: PrestamoActivoComponent},
+      {path: 'reg-devolucion', component: RegDevolucionComponent},
+      {path: 'reserva-lab', component: ReservaLabComponent},
+      {path: 'reportes', component: ReportesComponent}
+    ]
+  }
 
 ];
 
--- LabCE_web/LabCE/labce.client/src/app/app.module.ts
+++ LabCE_web/LabCE/labce.client/src/app/app.module.ts
@@ -4,12 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OperatorLoginComponent } from './operator-login/operator-login.component';
+import { OperatorLoginComponent } from './components/operator-login/operator-login.component';
+import { OperatorViewComponent } from './components/operator-view/operator-view.component';
+import { BodyComponent } from './components/operator-view/body/body.component';
+import { SidenavComponent } from './components/operator-view/sidenav/sidenav.component';
+import { PrestamoActivoComponent } from './components/operator-view/prestamo-activo/prestamo-activo.component';
+import { ReservaLabComponent } from './components/operator-view/reserva-lab/reserva-lab.component';
+import { RegDevolucionComponent } from './components/operator-view/reg-devolucion/reg-devolucion.component';
+import { ReportesComponent } from './components/operator-view/reportes/reportes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OperatorLoginComponent
+    OperatorLoginComponent,
+    OperatorViewComponent,
+    BodyComponent,
+    SidenavComponent,
+    PrestamoActivoComponent,
+    ReservaLabComponent,
+    RegDevolucionComponent,
+    ReportesComponent,
   ],
   imports: [
     BrowserModule, HttpClientModule,
