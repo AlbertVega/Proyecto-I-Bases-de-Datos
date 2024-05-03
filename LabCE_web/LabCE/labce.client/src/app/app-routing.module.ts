43f4b526a7e4dca68fd3240cd65b69c5ae37b6e8
DanielCob <83792193+DanielCob@users.noreply.github.com>
2024-05-03 15:58:46 -0600

    v1

--- LabCE_web/LabCE/labce.client/src/app/app-routing.module.ts
+++ LabCE_web/LabCE/labce.client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LabBookingComponent } from './lab-booking/lab-booking.component';
+import { EditableTableComponent } from './editable-table/editable-table.component';
 
 const routes: Routes = [
   { path: 'lab-booking', component: LabBookingComponent },
+  { path: 'editable-table', component: EditableTableComponent },
 ];
 
 @NgModule({
--- LabCE_web/LabCE/labce.client/src/app/app.module.ts
+++ LabCE_web/LabCE/labce.client/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LabBookingComponent } from './lab-booking/lab-booking.component';
+import { EditableTableComponent } from './editable-table/editable-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LabBookingComponent
+    LabBookingComponent,
+    EditableTableComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent, LabBookingComponent]
