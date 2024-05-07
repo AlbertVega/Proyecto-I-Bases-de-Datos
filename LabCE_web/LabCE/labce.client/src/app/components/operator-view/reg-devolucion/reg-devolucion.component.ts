4f0d7d770fc1aaee23300e64c8fe3801877ff2ce
Albert Vega camacho <85046754+AlbertVega@users.noreply.github.com>
2024-05-06 23:28:23 -0600

    devolucion-activo view

--- LabCE_web/LabCE/labce.client/src/app/components/operator-view/reg-devolucion/reg-devolucion.component.ts
+++ LabCE_web/LabCE/labce.client/src/app/components/operator-view/reg-devolucion/reg-devolucion.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './reg-devolucion.component.css'
 })
 export class RegDevolucionComponent {
+  rows = [
+    { placa:'2011283', tipo: 'Arduino', marca: 'ArduinoUNO', fecha: '04/05/2017' }
+  ];
 
+  editingRow: boolean[] = [];
+  selectedRow: number = -1;
+
+  constructor() {
+    for(let i = 0; i <this.rows.length; i++) {
+      this.editingRow.push(false);
+    }
+  }
+
+  selected(index: number) {
+    this.selectedRow = index;
+  }
+
+  removeSelection() {
+    this.selectedRow = -1;
+  }
 }
