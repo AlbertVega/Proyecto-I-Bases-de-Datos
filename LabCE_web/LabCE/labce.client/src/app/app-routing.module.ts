21c76504b0cf55e6c74c88da6ec120a335c359fc
Albert Vega camacho <85046754+AlbertVega@users.noreply.github.com>
2024-05-06 23:27:01 -0600

    Quitar ruta por defecto

--- LabCE_web/LabCE/labce.client/src/app/app-routing.module.ts
+++ LabCE_web/LabCE/labce.client/src/app/app-routing.module.ts
@@ -35,7 +35,6 @@ const routes: Routes = [
     ]
     
   },
-  { path: '', redirectTo: 'prof-login', pathMatch: 'full' }, 
   {
     path: 'op-register', component: OperatorRegisterComponent,
     children: [
